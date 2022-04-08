704ae94feed42bd84f8f4c0be5351bb98fd9c93b
Chris McDonough <chrism@plope.com>
2022-04-08 16:28:58 -0400

    add a volume argument

--- breakonthru/scripts/wavplayer.py
+++ breakonthru/scripts/wavplayer.py
@@ -2,10 +2,10 @@ import argparse
 import os
 import random
 
-def playrandom(directory, sink):
+def playrandom(directory, sink, volume):
     files = os.listdir(directory)
     fn = os.path.join(directory, random.choice(files))
-    os.system(f"paplay -d {sink} {fn}")
+    os.system(f"paplay -d {sink} --volume={volume} {fn}")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,5 +15,9 @@ def main():
     parser.add_argument(
         '--dir', help="Directory full of wav files",
         required=True)
+    parser.add_argument(
+        '--volume', help="Choose a value between 0 (silent) and 65536 (100% volume)",
+        type=int,
+        default=16384)
     args = parser.parse_args()
-    playrandom(args.dir, args.sink)
+    playrandom(args.dir, args.sink, args.volume)
