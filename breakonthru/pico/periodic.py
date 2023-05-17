b8a36d8acaf5c7644092c56d11a5389f53e8fff5
Chris McDonough <chrism@plope.com>
2023-05-17 14:25:58 -0400

    example of periodic timer

---
+++ breakonthru/pico/periodic.py
@@ -0,0 +1,17 @@
+import machine
+import time
+
+led = machine.Pin("LED")
+led.off()
+
+def blink(t):
+    print("blinking")
+    led.off()
+    time.sleep_ms(200)
+    led.on()
+    time.sleep_ms(200)
+
+t = machine.Timer(mode=machine.Timer.PERIODIC,
+                  freq=1, callback=blink)
+
+raise Exception("exc")
