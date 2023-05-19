0b898e2832b0ac65fe49c11ee2b83c55eba58e4a
Chris McDonough <chrism@plope.com>
2023-05-19 05:03:41 -0400

    make a little game

--- breakonthru/pico/7segment.py
+++ breakonthru/pico/7segment.py
@@ -1,4 +1,5 @@
 import machine
+import random
 import utime
 
 LEDS = {
@@ -47,33 +48,79 @@ def dp_blink():
         LEDS["DP"].off()
         utime.sleep(.05)
 
-try:
-    clear()
-    for x in range(1):
-        for name in order:
-            print(f"{name} on")
-            pin = LEDS[name]
-            pin.on()
-            utime.sleep(.1)
-            pin.off()
-            utime.sleep(.1)
+def snake():
+    for name in order:
+        pin = LEDS[name]
+        pin.on()
+        utime.sleep(.1)
+        pin.off()
+        utime.sleep(.1)
 
-    dp_blink()
-
-    for digit in range(10):
-        print(f"displaying {digit}")
-        display_digit(digit)
-        utime.sleep(1)
-
-    dp_blink()
-
+def boobs():
     for digit in (8, 0, 0, 8, 5):
         display_digit(digit)
         utime.sleep(.5)
         clear()
         utime.sleep(.5)
-    
+
+def display_digits():
+    for digit in range(10):
+         print(f"displaying {digit}")
+         display_digit(digit)
+         utime.sleep(1)
+
+clicks = 0
+last_click = 0
+
+def button_pressed(pin):
+    global clicks, last_click
+    new_time = utime.ticks_ms()
+    # debounce
+    if (new_time - last_click) > 100: 
+        clicks += 1
+        last_click = new_time
+
+buttonpin = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
+
+def game():
+    global clicks
+    clicks = 0
+    digits = range(1,9)
+    while True:
+        clear()
+        dp_blink()
+        clicks_before = clicks
+        digit = random.choice(digits)
+        buttonpin.irq(
+            trigger=machine.Pin.IRQ_FALLING,
+            handler = button_pressed
+            )
+        for x in range(10):
+            display_digit(digit)
+            utime.sleep_ms(400)
+            clear()
+            utime.sleep_ms(100)
+        buttonpin.irq(
+            trigger=machine.Pin.IRQ_FALLING,
+            handler = None
+            )
+        #print(f"clicks {clicks}, digit {digit}, clicks_before {clicks_before}")
+        dp_blink()
+        supplied = clicks - clicks_before
+        if supplied == digit:
+            print(f"Correct ({digit})")
+            snake()
+        else:
+            print(f"Incorrect (wanted {digit}, got {supplied})")
+
+
+try:
+    clear()
+    snake()
+    dp_blink()
+    game()    
 finally:
     for name, pin in LEDS.items():
         pin.off()
 
+    
