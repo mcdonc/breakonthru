c094cebb140cda503099c42a5a2a0d8fb32eb5ca
Chris McDonough <chrism@plope.com>
2023-05-22 23:31:16 -0400

    flail

--- breakonthru/pico/ledstrip.py
+++ breakonthru/pico/ledstrip.py
@@ -1,11 +1,11 @@
-from np import Neopixel
+from neopixel import NeoPixel
 import utime
-import random
 import sys
+import machine
 
 numpix = 144
 pin = 16
-strip = Neopixel(numpix, 0, pin)
+strip = NeoPixel(machine.Pin(pin), numpix)
 
 red = (255, 0, 0)
 orange = (255, 50, 0)
@@ -14,40 +14,77 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 indigo = (100, 0, 90)
 violet = (200, 0, 100)
+blank = (0,0,0)
+
 colors_rgb = [red, orange, yellow, green, blue, indigo, violet]
 
 delay = 0.5
-strip.brightness(42)
 blank = (0,0,0)
 
 divided = {
-    0: blue,
-    1: green,
+    0: yellow,
+    1: yellow,
     2: yellow,
-    3: red,
+    3: yellow,
 }
 
 divisor = 4
 divided_into = int(numpix / divisor)
 
-for x in range(divisor):
-    color = divided[x]
-        
-    start = (numpix//divisor) * x
-    end = start + (numpix//divisor)
+def segmented():
 
-    for pixel in range(start, end):
-        sys.stdout.write(f"{pixel} ")
-        sys.stdout.write(f"{color}\n")
-        strip.set_pixel(pixel, color)
-        utime.sleep_ms(1)
+    for x in range(divisor):
+        color = divided[x]
+            
+        start = (numpix//divisor) * x
+        end = start + (numpix//divisor)
 
-strip.show()
-utime.sleep_ms(5000)
-strip.fill((0,0,0))
-strip.show()
-strip.fill((0,0,0))
-strip.show()
+        for pixel in range(start, end):
+            sys.stdout.write(f"{pixel} ")
+            sys.stdout.write(f"{color}\n")
+            strip[pixel] = color
+    strip.write()
+
+def clear():
+    strip.fill(blank)
+    strip.write()
+
+audio = machine.ADC(machine.Pin(26, machine.Pin.IN))
+
+def get_audio_level():
+    level = audio.read_u16()
+    return level
+
+try:
+    while True:
+        level = get_audio_level()
+        percent = 65535 / level
+        print(percent)
+        start = 0
+        end = int(numpix * (percent/100))
+        leds = list(range(start, end))
+        print(leds)
+        for x in leds:
+            strip[x] = yellow
+        strip.write()
+        #utime.sleep_ms(100)
+        clear()
+finally:
+    strip.fill(blank)
+    strip.write()
+
+
+# try:
+#     segmented()
+#     utime.sleep(5)
+# finally:
+#     clear()
+
+#utime.sleep_ms(5000)
+#strip.fill((0,0,0))
+#strip.show()
+#strip.fill((0,0,0))
+#strip.show()
 # try:
 #     while True:
 #         strip.set_pixel(random.randint(0, numpix-1), colors_rgb[random.randint(0, len(colors_rgb)-1)])
